d14de136cd77305ef37f26b063534148e5f0c5ae
Marcuslyj <marcuslyj999@gmail.com>
2020-02-07 16:45:46 +0800

    操作cookie

--- app.js
+++ app.js
@@ -41,6 +41,20 @@ const serverHandle = (req, res) => {
     // 解析query
     req.query = querystring.parse(url.split('?')[1])
 
+    // 解析cookie
+    req.cookie = {}
+    const cookieStr = req.headers.cookie || ''
+    cookieStr.split(';').forEach(item => {
+        if (!item) {
+            return
+        }
+        const arr = item.split('=')
+        const key = arr[0]
+        const value = arr[1]
+        req.cookie[key] = value
+    })
+    console.log('cookie', req.cookie)
+
     // 处理postData
     getPostData(req).then(postData => {
         req.body = postData
--- src/router/user.js
+++ src/router/user.js
@@ -1,4 +1,4 @@
-const { loginCheck } = require('../controller/user')
+const { login } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
 const handleUserRouter = (req, res) => {
@@ -8,13 +8,23 @@ const handleUserRouter = (req, res) => {
     // 登录
     if (method === 'GET' && path === '/api/user/login') {
         const { username, password } = req.body
-        const result = loginCheck(username, password)
+        const result = login(username, password)
         return result.then(data => {
             if (data.username) {
+                // 操作cookie
+                res.setHeader('Set-Cookie', `username=${data.username};path=/`)
                 return new SuccessModel()
             }
             return new ErrorModel('登录失败')
         })
+    }
+
+    // 登录验证
+    if (method === 'GET' && req.path === '/api/user/login-test') {
+        if (req.cookie.username) {
+            return Promise.resolve(new SuccessModel())
+        }
+        return Promise.resolve(new ErrorModel('尚未登录'))
     }
 }
 
