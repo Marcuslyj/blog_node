838ebfd57566f8a6fba0ec3bca2828ed06b5bbc3
Marcuslyj <marcuslyj999@gmail.com>
2020-02-09 22:49:21 +0800

    设置cookie过期时间

--- src/router/user.js
+++ src/router/user.js
@@ -1,21 +1,25 @@
 const { login } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
 
+// 获取cookie的过期时间
+const getCookieExpires = () => {
+    const d = new Date()
+    d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
+    return d.toGMTString()
+}
+
 const handleUserRouter = (req, res) => {
     const method = req.method
     const path = req.path
 
     // 登录
     if (method === 'POST' && path === '/api/user/login') {
-        console.log('((((((((((((((((((((((((((((((')
-        console.log(req.body)
-
         const { username, password } = req.body
         const result = login(username, password)
         return result.then(data => {
             if (data.username) {
                 // 操作cookie
-                res.setHeader('Set-Cookie', `username=${data.username};path=/;httpOnly`)
+                res.setHeader('Set-Cookie', `username=${data.username};path=/;httpOnly;expires=${getCookieExpires()}`)
                 return new SuccessModel()
             }
             return new ErrorModel('登录失败')
