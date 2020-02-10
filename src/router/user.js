d9542d93336156346879de2e8a9f59047146a1f1
Marcuslyj <marcuslyj999@gmail.com>
2020-02-10 15:57:46 +0800

    删除login-test接口

--- src/router/user.js
+++ src/router/user.js
@@ -18,16 +18,6 @@ const handleUserRouter = (req, res) => {
             return new ErrorModel('登录失败')
         })
     }
-
-    // 登录验证
-    if (method === 'GET' && req.path === '/api/user/login-test') {
-        if (req.session.username) {
-            return Promise.resolve(new SuccessModel({
-                username: req.session.username
-            }))
-        }
-        return Promise.resolve(new ErrorModel('尚未登录'))
-    }
 }
 
 module.exports = handleUserRouter
