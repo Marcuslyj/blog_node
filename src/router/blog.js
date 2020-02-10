bcc060a557903e05ffdc764c0424183eda549bce
Marcuslyj <marcuslyj999@gmail.com>
2020-02-10 21:08:00 +0800

    登录信息存到redis；nginx反向代理，处理前后端不同域名问题

--- src/router/blog.js
+++ src/router/blog.js
@@ -21,8 +21,20 @@ const handleBlogRouter = (req, res) => {
 
     // 获取博客列表
     if (method === 'GET' && req.path === '/api/blog/list') {
-        const author = req.query.author || ''
-        const keyword = req.query.keyword || ''
+        let author = req.query.author || ''
+        let keyword = req.query.keyword || ''
+
+        if (req.query.isadmin) {
+            // 管理员界面
+            const loginCheckResult = loginCheck(req)
+            console.log(req)
+            if (loginCheckResult) {
+                // 未登录
+                return loginCheckResult
+            }
+            // 强制查询自己的博客
+            author = req.session.username
+        }
 
         const result = getList(author, keyword)
 
@@ -42,10 +54,11 @@ const handleBlogRouter = (req, res) => {
 
     // 新建博客
     if (method === 'POST' && path === '/api/blog/new') {
+        console.log(req)
         const loginCheckResult = loginCheck(req)
         if (loginCheckResult) {
             // 未登录
-            return loginCheck
+            return loginCheckResult
         }
 
         req.body.author = req.session.username
@@ -61,7 +74,7 @@ const handleBlogRouter = (req, res) => {
         const loginCheckResult = loginCheck(req)
         if (loginCheckResult) {
             // 未登录
-            return loginCheck
+            return loginCheckResult
         }
 
         const result = updateBlog(id, req.body)
@@ -79,7 +92,7 @@ const handleBlogRouter = (req, res) => {
         const loginCheckResult = loginCheck(req)
         if (loginCheckResult) {
             // 未登录
-            return loginCheck
+            return loginCheckResult
         }
 
         // 假数据
--- src/router/user.js
+++ src/router/user.js
@@ -1,5 +1,6 @@
 const { login } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+const { set } = require('../db/redis')
 
 const handleUserRouter = (req, res) => {
     const method = req.method
@@ -13,6 +14,8 @@ const handleUserRouter = (req, res) => {
             if (data.username) {
                 req.session.username = data.username
                 req.session.realname = data.realname
+                // 同步到 redis
+                set(req.sessionId, req.session)
                 return new SuccessModel()
             }
             return new ErrorModel('登录失败')
