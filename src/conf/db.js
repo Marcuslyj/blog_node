41c53857825b184b9951feb3ec16f758c729024d
Marcuslyj <marcuslyj999@gmail.com>
2020-02-10 13:55:08 +0800

    redis配置和操作函数封装

--- src/conf/db.js
+++ src/conf/db.js
@@ -2,6 +2,7 @@ const env = process.env.NODE_ENV
 
 // 配置
 let MYSQL_CONF
+let REDIS_CONF
 
 if (env === 'development') {
     MYSQL_CONF = {
@@ -10,6 +11,11 @@ if (env === 'development') {
         password: '',
         port: '3306',
         database: 'myblog'
+    }
+    // redis
+    REDIS_CONF = {
+        port: 6379,
+        host: '127.0.0.1'
     }
 }
 
@@ -21,8 +27,14 @@ if (env === 'production') {
         port: '3306',
         database: 'myblog'
     }
+    // redis
+    REDIS_CONF = {
+        port: 6379,
+        host: '127.0.0.1'
+    }
 }
 
 module.exports = {
-    MYSQL_CONF
+    MYSQL_CONF,
+    REDIS_CONF
 }
