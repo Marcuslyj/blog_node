14a5e6432e1f96cd05f2809c33674e2b1e138994
Marcuslyj <marcuslyj999@gmail.com>
2020-02-11 21:09:17 +0800

    安全问题：所有用来拼接sql语句的变量，用escape处理（处理后，不用到单引号）

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,8 +1,12 @@
-const { exec } = require('../db/mysql')
+const { exec, escape } = require('../db/mysql')
 
 const login = (username, password) => {
+    // 防sql注入
+    username = escape(username)
+    password = escape(password)
+
     const sql = `
-        select username, realname from users where  username='${username}' and password='${password}'
+        select username, realname from users where  username=${username} and password=${password}
     `
     return exec(sql).then(rows => {
         return rows[0] || {}
