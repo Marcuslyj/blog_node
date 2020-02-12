de995f9dcbce29bcd861b6ce2ec3b5a19c3a1d75
Marcuslyj <marcuslyj999@gmail.com>
2020-02-12 14:37:47 +0800

    入口文件解析

--- blog-express/app.js
+++ blog-express/app.js
@@ -7,22 +7,31 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// 初始化app
 var app = express();
 
+// 模板引擎设置
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// 写日志
 app.use(logger('dev'));
+// 获取post的json数据
 app.use(express.json());
+// 其他数据格式
 app.use(express.urlencoded({ extended: false }));
+// 解析cookie，可通过req.cookies访问
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// 静态文件
+// app.use(express.static(path.join(__dirname, 'public')));
 
+// 路由
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
+// 处理404
 app.use(function(req, res, next) {
   next(createError(404));
 });
