bc23bc054adf940cea7dc39037fe33cd6f7bcd76
Marcuslyj <marcuslyj999@gmail.com>
2020-02-12 15:03:59 +0800

    处理get和post请求

--- blog-express/app.js
+++ blog-express/app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var blogRouter = require('./routes/blog')
+var userRouter = require('./routes/user');
 
 // 初始化app
 var app = express();
@@ -29,15 +31,17 @@ app.use(cookieParser());
 // 路由
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/blog', blogRouter)
+app.use('/api/user', userRouter)
 
 // catch 404 and forward to error handler
 // 处理404
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
